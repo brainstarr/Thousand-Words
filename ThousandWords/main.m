b75a554efde98b32a877656eeadb1cf009d0489e
BrainStarr <brian.r.starr@gmail.com>
2014-11-05 15:08:20 -0800

    Initial Commit

---
+++ ThousandWords/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThousandWords
+//
+//  Created by Brian Starr on 11/5/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ThousandWordsTests/ThousandWordsTests.m
@@ -0,0 +1,40 @@
+//
+//  ThousandWordsTests.m
+//  ThousandWordsTests
+//
+//  Created by Brian Starr on 11/5/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ThousandWordsTests : XCTestCase
+
+@end
+
+@implementation ThousandWordsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
