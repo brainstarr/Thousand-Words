570985f01a9b343ea903c6fba4ab909bbedbcd9c
BrainStarr <brian.r.starr@gmail.com>
2014-11-05 15:52:14 -0800

    added cell configuration basics, lazy instantiation for albums

--- ThousandWords/AlbumTableViewController.m
+++ ThousandWords/AlbumTableViewController.m
@@ -14,6 +14,12 @@
 
 @implementation AlbumTableViewController
 
+-(NSMutableArray *)albums
+{
+    if (!_albums) _albums = [[NSMutableArray alloc]init];
+    return _albums;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -32,20 +38,20 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
+
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
+
     // Return the number of rows in the section.
-    return 0;
+    return [self.albums count];
 }
 
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: forIndexPath:indexPath];
     
     // Configure the cell...
     
