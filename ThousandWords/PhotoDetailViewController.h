a4eaace65eea62942c83529254d93d9c50edb08c
BrainStarr <brian.r.starr@gmail.com>
2014-11-18 21:07:28 -0800

    added photo detial VC.

---
+++ ThousandWords/PhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailViewController.h
+//  ThousandWords
+//
+//  Created by Brian Starr on 11/18/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoDetailViewController : UIViewController
+
+@end
